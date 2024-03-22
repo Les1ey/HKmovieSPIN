0578a38c77938fbcfba115b7fc44deb4068bae53
HanaBear <138543737+Les1ey@users.noreply.github.com>
2024-03-22 16:16:08 +0800

    Update main.py
    调整返回的提示语

--- main.py
+++ main.py
@@ -6,19 +6,19 @@ actor_pool = ['刘青云', '吴镇宇', '古天乐', '张智霖', '黄秋生', '
 
 # 随机选择3个演员并打印
 selected_actors = []
-# 修改下面range里面的数字可以控制抽几个演员
+# 修改下面range里面的数字可以控制抽几个演员刘伟强
 for i in range(2):
     actor = random.choice(actor_pool)
     actor_pool.remove(actor)
     selected_actors.append(actor)
-    print('抽到的第{}位演员是：{}'.format(i + 1, actor))
+    print('抽出来第{}位是：{}'.format(i + 1, actor))
 
 selected_movies = []
 for movie, info in movies.items():
     if set(selected_actors).issubset(set(info['actors'])):
-        selected_movies.append(movie + ', 导演是：' + info['director'])
+        selected_movies.append(movie + ', 导演：' + info['director'])
 
 if selected_movies:
     print('他们共同参演的电影有: ', ', '.join(selected_movies))
 else:
-    print('遗憾的是，他们没有共同参演过电影。')
+    print('电影库里没找到他们合作的电影呢。')
